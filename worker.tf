144866a2e124482d7ab5baf626b2830d7b3530e4
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-02-04 15:41:41 +0100

    Update worker count

--- worker.tf
+++ worker.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "worker" {
-  count = 3
+  count = 2
   name = "tf-worker${count.index+1}"
   image_id = "406238e3-bac8-4e78-80ac-911d47d46c14"
   flavor_name = "gx1.1c1r"
