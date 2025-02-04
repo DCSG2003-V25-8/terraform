47aa0fe39fe16797ada3d290766921df53917e76
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-02-04 15:38:34 +0100

    Fix naming

--- ingress.tf
+++ ingress.tf
@@ -2,18 +2,18 @@ resource "openstack_networking_floatingip_v2" "floatip_1" {
   pool = "ntnu-internal"
 }
 
-data "openstack_networking_port_v2" "vm-port" {
-  device_id  = openstack_compute_instance_v2.worker.id
+data "openstack_networking_port_v2" "ingress-port" {
+  device_id  = openstack_compute_instance_v2.ingress.id
   network_id = "35fe8b76-cc31-4b48-b3f0-5c48eef8d289"
 }
 
-resource "openstack_networking_floatingip_associate_v2" "myip" {
+resource "openstack_networking_floatingip_associate_v2" "ingressIP" {
   floating_ip = openstack_networking_floatingip_v2.floatip_1.address
-  port_id     = data.openstack_networking_port_v2.vm-port.id
+  port_id     = data.openstack_networking_port_v2.ingress-port.id
 }
 
 
-resource "openstack_compute_instance_v2" "worker" {
+resource "openstack_compute_instance_v2" "ingress" {
   name = "tf-ingress"
   image_id = "406238e3-bac8-4e78-80ac-911d47d46c14"
   flavor_name = "gx1.1c1r"
