3cc27e6f351a948e9d0b115447bc27b7ab56fff3
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-02-04 12:06:33 +0100

    Cloud init

--- loadbalancer.tf
+++
@@ -1,8 +0,0 @@
-resource "openstack_compute_instance_v2" "loadbalancer" {
-  name            = "Terraform loadbalancer"
-  image_name        = "Ubuntu Server 22.04 LTS (Jammy Jellyfish) amd64"
-  flavor_name       = "gx1.1c1r"
-  key_pair = openstack_compute_keypair_v2.MasterKey.name
-  security_groups = ["default"]
-  power_state = "active"
-}
--- worker.tf
+++ worker.tf
@@ -1,5 +1,6 @@
-resource "openstack_compute_instance_v2" "name" {
-  name = "Terraform db"
+resource "openstack_compute_instance_v2" "worker" {
+  count = 3
+  name = "tf-worker${count.index+1}"
   image_name = "Ubuntu Server 22.04 LTS (Jammy Jellyfish) amd64"
   flavor_name = "gx1.1c1r"
   key_pair = openstack_compute_keypair_v2.MasterKey.name
