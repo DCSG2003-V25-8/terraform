60689b5d1466babf2fb485a28ab3291d416ba310
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-02-04 10:09:24 +0100

    Added state

--- db.tf
+++ db.tf
@@ -1,6 +1,5 @@
-resource "openstack_compute_instance_v2" "webServer" {
-  count = 2
-  name = "Terraform webserver${count.index+1}"
+resource "openstack_compute_instance_v2" "name" {
+  name = "Terraform db"
   image_name = "Ubuntu Server 22.04 LTS (Jammy Jellyfish) amd64"
   flavor_name = "gx1.1c1r"
   key_pair = openstack_compute_keypair_v2.MasterKey.name
--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,7 +1,3 @@
-locals {
-  haproxy_config = base64encode(templatefile("${path.module}/files/loadbalancer/haproxy.cfg.tftpl", {ips = openstack_compute_instance_v2.webServer.*.access_ip_v4}))
-}
-
 resource "openstack_compute_instance_v2" "loadbalancer" {
   name            = "Terraform loadbalancer"
   image_name        = "Ubuntu Server 22.04 LTS (Jammy Jellyfish) amd64"
