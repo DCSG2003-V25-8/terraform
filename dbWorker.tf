3b6ab11407b573dfdb0efb8f80cc5f5819698b06
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-02-11 14:21:31 +0100

    Added volume to db

--- dbWorker.tf
+++ dbWorker.tf
@@ -1,10 +1,14 @@
 resource "openstack_compute_instance_v2" "db_worker" {
-  count = 1
-  name = "tf-db-worker${count.index+1}"
+  name = "tf-db-worker"
   image_id = "406238e3-bac8-4e78-80ac-911d47d46c14"
   flavor_name = "gx1.1c1r"
   key_pair = openstack_compute_keypair_v2.Manager.name
   security_groups = ["default"]
   power_state = "active"
   user_data = file("./dbWorker.yaml")
+}
+
+resource "openstack_compute_volume_attach_v2" "vl_1" {
+  instance_id = openstack_compute_instance_v2.db_worker.id
+  volume_id = "7a20a40e-6aac-4441-9956-1f701a42d789"
 }
